475eb42132e38ea7883310c66dd025a5bbdfe8dc
Graham Lutz <graham.lutz@gmail.com>
2015-09-22 18:47:21 -0400

    Stylize gear list table

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,18 +3,26 @@ var app = angular.module('virtualPack', []);
 app.controller('MainCtrl', ['$scope', function($scope) {
 	$scope.test = 'Scope test';
 	$scope.products = [
-		{manufacturer: 'Enlightened Equipment', name: 'Revelation', weight: 21.95, units: 'oz', nickname: 'Top Quilt'},
-		{manufacturer: 'Enlightened Equipment', name: 'Revolt', weight: 21.95, units: 'oz', nickname: 'UQ'},
-		{manufacturer: 'Osprey', name: 'Exos 58', weight: 39, units: 'oz', nickname: 'Backpack'}
+		{manufacturer: 'Enlightened Equipment', name: 'Revelation', weight: 21.95, units: 'oz', nickname: 'Top Quilt', price: 295},
+		{manufacturer: 'Enlightened Equipment', name: 'Revolt', weight: 21.95, units: 'oz', nickname: 'UQ', price: 255},
+		{manufacturer: 'Osprey', name: 'Exos 58', weight: 39, units: 'oz', nickname: 'Backpack', price: 199}
 	];
 	$scope.addProduct = function() {
+		var p = $scope.products;
+		//if(!p.manufacturer || p.manufacturer === '') { return; }
 		$scope.products.push({
 			manufacturer: $scope.products.manufacturer, 
 			name: $scope.products.name, 
 			weight: $scope.products.weight, 
 			units: $scope.products.units, 
-			nickname: $scope.products.nickname
+			nickname: $scope.products.nickname,
+			price: $scope.products.price
 		});
-		console.log("manufacturer: " + $scope.products.manufacturer)
+		p.manufacturer = '';
+		p.name = '';
+		p.weight = '';
+		p.units = 'oz';
+		p.nickname = '';
+		p.price = '';
 	}
 }])
