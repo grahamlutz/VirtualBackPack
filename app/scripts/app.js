70a99ec50b27317d751a52a91365a49e1fd6eaf7
Graham Lutz <graham.lutz@gmail.com>
2015-09-22 16:04:12 -0400

    Create addProduct() functionality

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -7,4 +7,14 @@ app.controller('MainCtrl', ['$scope', function($scope) {
 		{manufacturer: 'Enlightened Equipment', name: 'Revolt', weight: 21.95, units: 'oz', nickname: 'UQ'},
 		{manufacturer: 'Osprey', name: 'Exos 58', weight: 39, units: 'oz', nickname: 'Backpack'}
 	];
+	$scope.addProduct = function() {
+		$scope.products.push({
+			manufacturer: $scope.products.manufacturer, 
+			name: $scope.products.name, 
+			weight: $scope.products.weight, 
+			units: $scope.products.units, 
+			nickname: $scope.products.nickname
+		});
+		console.log("manufacturer: " + $scope.products.manufacturer)
+	}
 }])
