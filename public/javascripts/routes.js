f90a76140a816501059c733b22a5526997c40134
Graham Lutz <graham@brainbytescreative.com>
2016-08-29 14:22:18 -0400

    import angular front end

---
+++ public/javascripts/routes.js
@@ -0,0 +1,16 @@
+// var app = angular.module('myVirtualPack');
+//
+// app.config([
+//   '$stateProvider',
+//   '$urlRouterProvider',
+//   function($stateProvider, $urlRouterProvider) {
+//     $stateProvider
+//       .state('home', {
+//         url:'/home',
+//         templateUrl: 'views/home.html',
+//         controller: 'MainCtrl'
+//       });
+//
+//     $urlRouterProvider.otherwise('home');
+//   }
+// ])
