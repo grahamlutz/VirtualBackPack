088e7fa370bb25a8ec524eaecb992aaf93542235
Graham Lutz <graham@brainbytescreative.com>
2016-08-29 16:46:30 -0400

    add route to upvote post

--- routes/index.js
+++ routes/index.js
@@ -28,7 +28,6 @@ router.post('/posts', function(req, res, next) {
 });
 
 /* preload post */
-
 router.param('post', function(req, res, next, id) {
   var query = Post.findById(id);
 
@@ -44,6 +43,14 @@ router.param('post', function(req, res, next, id) {
 /* GET single post */
 router.get('/posts/:post', function(req, res) {
   res.json(req.post);
+});
+
+/* PUT upvote a post */
+router.put('/posts/:post/upvote', function(req, res, next) {
+  req.post.upvote(function(err, post) {
+    if (err) return next(err);
+    res.json(post);
+  })
 });
 
 module.exports = router;
