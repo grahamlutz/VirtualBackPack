073474b134c7eae8ddae50b689a943d0d66c01bc
Graham Lutz <graham@brainbytescreative.com>
2016-08-31 10:28:52 -0400

    stub out addGear on home page

--- public/scripts/controllers/mainCtrl.js
+++ public/scripts/controllers/mainCtrl.js
@@ -2,16 +2,36 @@ var app = angular.module( 'myVirtualPack');
 
 app.controller( 'MainCtrl', ['$scope', 'posts', 'gear', 'auth', mainCtrl]);
 
-function mainCtrl ($scope, posts, auth) {
+function mainCtrl ($scope, posts, gear, auth) {
   $scope.posts = posts.posts;
+  $scope.gear = gear.items;
   $scope.isLoggedIn = auth.isLoggedIn;
-  
+
   $scope.addGear = addGear;
   $scope.addPost = addPost;
   $scope.incrementUpvotes = incrementUpvotes;
   $scope.decrementUpvotes = decrementUpvotes;
 
-  function addGear() {};
+  function addGear() {
+    console.log('addGear fired');
+    var g = $scope.gear;
+  	if(!g.manufacturer || g.manufacturer === '') { return; }
+  	$scope.gear.push({
+  		manufacturer: $scope.gear.manufacturer,
+  		name: g.name,
+  		weight: g.weight,
+  		units: g.units,
+  		nickname: $scope.gear.nickname,
+  		price: g.price
+  	});
+  	console.log(g[0].manufacturer);
+  	g.manufacturer = '';
+  	g.name = '';
+  	g.weight = '';
+  	g.units = 'oz';
+  	g.nickname = '';
+  	g.price = '';
+  };
 
   function addPost() {
     if(!$scope.title || $scope.title === '') { return; }
