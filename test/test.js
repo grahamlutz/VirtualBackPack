376d0ff721de8ce93e18fb806a9d09935149c08a
Graham Lutz <graham@brainbytescreative.com>
2016-09-10 20:24:48 -0400

    sets up GET /gear and GET gear/:item tests - passing

--- test/test.js
+++ test/test.js
@@ -8,24 +8,38 @@ var server = require('../app');
 chai.use(chaiHttp);
 
 describe('Gear', function() {
-  it('should list ALL gear on /gear GET', function(done) {
-    expect(true).to.equal(false);
-    done();
+    it('should list ALL gear on /gear GET', function(done) {
+      chai.request(server)
+      .get('/gear')
+      .end(function(err, res){
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('array');
+        res.body.length.should.be.above(0);
+        done();
+      });
+    });
+  it('should list a SINGLE gear item on /gear/<item> GET', function(done) {
+    chai.request(server)
+    .get('/gear/57c75e85004598ce935aa39b')
+    .end(function(err, res){
+      res.should.have.status(200);
+      res.should.be.json;
+      res.body.should.be.a('object');
+      res.body.should.have.property('_id');
+      done();
+    });
   });
-  it('should list a SINGLE blob on /gear/<item> GET', function(done) {
-    expect(true).to.equal(false);
-    done();
-  });
-  it('should add a SINGLE blob on /gear POST', function(done) {
-    expect(true).to.equal(false);
-    done();
-  });
-  it('should update a SINGLE blob on /gear/<item> PUT', function(done) {
-    expect(true).to.equal(false);
-    done();
-  });
-  it('should delete a SINGLE blob on /gear/<item>/delete DELETE', function(done) {
-    expect(true).to.equal(false);
-    done();
-  });
+  // it('should add a SINGLE gear item on /gear POST', function(done) {
+  //   expect(true).to.equal(false);
+  //   done();
+  // });
+  // it('should update a SINGLE gear item on /gear/<item> PUT', function(done) {
+  //   expect(true).to.equal(false);
+  //   done();
+  // });
+  // it('should delete a SINGLE gear item on /gear/<item>/delete DELETE', function(done) {
+  //   expect(true).to.equal(false);
+  //   done();
+  // });
 });
