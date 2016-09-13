8b84fd28c290d926ec7188f30b5b9c915200644e
Graham Lutz <graham@brainbytescreative.com>
2016-09-13 15:33:52 -0400

    adds favicon to /public/ folder and updates favicon middleware

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // TODO: add favicon
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
