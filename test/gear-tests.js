3ff64de3d4b7be730b0c1ac6d9faa52bef0af53e
Graham Lutz <graham@brainbytescreative.com>
2016-09-13 16:19:04 -0400

    creates test for add Gear /gear POST

--- test/gear-tests.js
+++ test/gear-tests.js
@@ -9,6 +9,8 @@ module.exports = function() {
   var chaiHttp = require('chai-http');
   var mongoose = require('mongoose');
   var server = require('../app');
+  var jwt = require('express-jwt');
+  var auth = jwt({secret: 'SECRET', userProperty: 'payload'});
   var Gear = mongoose.model('Gear');
 
   chai.use(chaiHttp);
@@ -49,20 +51,35 @@ module.exports = function() {
 
     it('should list a SINGLE gear item on /gear/<item> GET', function(done) {
       chai.request(server)
-      .get('/gear/57c75e85004598ce935dd48c')
-      .end(function(err, res){
-        res.should.have.status(200);
-        res.should.be.json;
-        res.body.should.be.a('object');
-        res.body.should.have.property('_id');
-        done();
+        .get('/gear/57c75e85004598ce935dd48c')
+        .end(function(err, res){
+          res.should.have.status(200);
+          res.should.be.json;
+          res.body.should.be.a('object');
+          res.body.should.have.property('_id');
+          done();
       });
     });
 
-    // it('should add a SINGLE gear item on /gear POST', function(done) {
-    //   expect(true).to.equal(false);
-    //   done();
-    // });
+    it('should add a SINGLE gear item on /gear POST', function(done) {
+      chai.request(server)
+        .post('/gear')
+        .set('Authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1N2NjOTQ0OWVkODhmYmU0YzY1MTdkNjMiLCJ1c2VybmFtZSI6ImFkbWluMiIsImV4cCI6MTQ3ODk4MTMxNywiaWF0IjoxNDczNzkzNzE3fQ.zy4n0W4d2isiQLkad5I97ea9McspwHEVQQPmWr6H3pM')
+        .send({'name': 'test product', 'manufacturer':'test man'})
+        .end(function(err, res){
+          res.should.have.status(200);
+          res.should.be.json;
+          res.body.should.be.a('object');
+          //res.body.should.have.property('SUCCESS');
+          res.body.should.be.a('object');
+          res.body.should.have.property('name');
+          res.body.should.have.property('manufacturer');
+          res.body.should.have.property('_id');
+          res.body.name.should.equal('test product');
+          res.body.manufacturer.should.equal('test man');
+          done();
+        });
+      });
 
     // it('should update a SINGLE gear item on /gear/<item> PUT', function(done) {
     //   expect(true).to.equal(false);
